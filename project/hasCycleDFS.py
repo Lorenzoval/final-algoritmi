897643e0937021c65eb00747f9d005b943c3032a
Lorenzo Valeriani <lorenzoval98@gmail.com>
2019-01-13 12:46:11 +0100

    hasCycleDFS: add algorithm

---
+++ project/hasCycleDFS.py
@@ -0,0 +1,52 @@
+import project.GraphGenerator as GraphGenerator
+import graph.Graph_AdjacencyList
+import random
+
+
+def hasCycleDFS(G):
+    """
+    Esegue una visita DFS nel grafo G a partire da un nodo random. Mantiene bool di marcatura per ogni nodo
+    tramite indicizzazione diretta su una lista, verifica se ci sono archi all'indietro, ovvero da un nodo
+    ad uno già visitato che non ne sia il padre. Se non ci sono archi all'indietro il grafo è aciclico.
+    :param G: Graph.
+    :return: bool.
+    """
+    rootId = random.choice(list(G.nodes.keys()))  # inizia la visita da un nodo random
+    visited = [False for _ in G.nodes]  # inizializza la lista di marcatura
+    return hasCycleDFSRecursive(G, rootId, visited, rootId)  # inizia la visita vera e propria
+
+
+def hasCycleDFSRecursive(G, v, visited, fatherId):
+    visited[v] = True  # marca il nodo v come visitato
+    for adjNode in G.getAdj(v):
+        if not visited[adjNode]:
+            if hasCycleDFSRecursive(G, adjNode, visited, v):  # se la chiamata ricorsiva individua un ciclo
+                return True  # ritorna True
+        elif visited[adjNode] and adjNode != fatherId:
+            return True
+
+    return False
+
+
+def printResults(G):
+    if hasCycleDFS(G):
+        print("The graph has at least one cycle")
+    else:
+        print("The graph has no cycle")
+
+
+if __name__ == "__main__":
+    print("Acyclic:")
+    acyclic = graph.Graph_AdjacencyList.GraphAdjacencyList()
+    GraphGenerator.generateAcyclicGraph(acyclic, 10)
+    printResults(acyclic)
+
+    print("Cyclic:")
+    cyclic = graph.Graph_AdjacencyList.GraphAdjacencyList()
+    GraphGenerator.generateCyclicGraphNaive(cyclic, 10)
+    printResults(cyclic)
+
+    print("Random:")
+    rand = graph.Graph_AdjacencyList.GraphAdjacencyList()
+    print("Number of edges:", GraphGenerator.generateRandGraph(rand, 10))
+    printResults(rand)
