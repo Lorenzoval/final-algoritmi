7bab8a936c6d9d57b4a04b0de190d49becb6890d
Lorenzo Valeriani <lorenzoval98@gmail.com>
2019-01-14 01:03:04 +0100

    hasCycleUF: add algorithm based on QuickUnionBalanced

--- project/hasCycleUF.py
+++ project/hasCycleUF.py
@@ -58,8 +58,34 @@ def hasCycleUFTest(G, uf):
     return False
 
 
-def printResults(G, uf):
-    if hasCycleUFTest(G, uf):
+def hasCycleUF(G):
+    """
+    Controlla se il grafo ha un ciclo sfruttando la struttura dati Union-Find.
+    :param G: Graph.
+    :return: bool.
+    """
+    uf = unionfind.quickUnion.QuickUnionBalanced()
+    for n in G.nodes.keys():
+        uf.makeSet(n)
+    marked = set()
+    for edge in EdgesIterator(G):
+        if (edge.head, edge.tail) in marked:
+            continue
+        else:
+            # aggiungi solo l'arco (edge.tail, edge.head) a marked, dato che (edge.head, edge.tail)
+            # non verrà più generato dall'iterator
+            marked.add((edge.tail, edge.head))
+        if uf.find(uf.nodes[edge.head]) == uf.find(uf.nodes[edge.tail]):
+            return True
+
+        else:
+            uf.union(uf.findRoot(uf.nodes[edge.head]), uf.findRoot(uf.nodes[edge.tail]))
+
+    return False
+
+
+def printResults(G, func, *otherParameters):
+    if func(G, *otherParameters):
         print("The graph has at least one cycle")
     else:
         print("The graph has no cycle")
@@ -69,17 +95,19 @@ if __name__ == "__main__":
     print("Acyclic:")
     acyclic = graph.Graph_AdjacencyList.GraphAdjacencyList()
     GraphGenerator.generateAcyclicGraph(acyclic, 10)
-    qU = unionfind.quickUnion.QuickUnion()
-    printResults(acyclic, qU)
+    printResults(acyclic, hasCycleUF)
 
     print("Cyclic:")
     cyclic = graph.Graph_AdjacencyList.GraphAdjacencyList()
     GraphGenerator.generateCyclicGraphNaive(cyclic, 10)
-    qU2 = unionfind.quickUnion.QuickUnion()
-    printResults(cyclic, qU2)
+    printResults(cyclic, hasCycleUF)
 
     print("Random:")
     rand = graph.Graph_AdjacencyList.GraphAdjacencyList()
     print("Number of edges:", GraphGenerator.generateRandGraph(rand, 10))
-    qU3 = unionfind.quickUnion.QuickUnion()
-    printResults(rand, qU3)
+    printResults(rand, hasCycleUF)
+
+    print("Complete:")
+    comp = graph.Graph_AdjacencyList.GraphAdjacencyList()
+    GraphGenerator.generateCompleteGraph(comp, 10)
+    printResults(comp, hasCycleUF)
